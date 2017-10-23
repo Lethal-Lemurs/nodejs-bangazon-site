d36bce22f424dffb8ebd35ebbb4e90693472cd1b
Glen Vollmer <glenvollmer@Glens-Macbook-Pro.local>
2017-10-23 14:29:32 -0500

    touched the models for prod type, made timestamps false

--- models/product_type.js
+++ models/product_type.js
@@ -2,7 +2,7 @@
 module.exports = (sequelize, DataTypes) => {
   var Product_type = sequelize.define('Product_type', {
     name: DataTypes.STRING
-  });
+  }, {timestamps: false});
 
   Product_type.associate = (models) => {
    Product_type.hasOne(models.Product);
